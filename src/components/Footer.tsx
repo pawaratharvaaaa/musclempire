e6944a9426b8518366924f4e23627f5b2124b1fd
atharvaaa pawar <pawaratharvaak@gmail.com>
2026-08-21 13:55:24 +0530

    style: use specific original logo in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { FaInstagram, FaFacebookF } from "react-icons/fa";
-import logo from "@/assets/images/logo.png";
+import logo from "@/assets/images/logo-footer.png";
 
 const quickLinks = [
   { href: "/branches", label: "Branches" },
